60b79f7e9c85314be68d1227059bfdf07e42cd41
Rengar Lee <Rengarlee@163.com>
2025-08-10 22:28:23 +0800

    Refactor code structure for improved readability and maintainability

--- libs/GeoDistrictDetector/Services/IGeoDistrictService.cs
+++ libs/GeoDistrictDetector/Services/IGeoDistrictService.cs
@@ -1,30 +1,33 @@
-using GeoLocationCityDetector.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GeoDistrictDetector.Models;
 
-namespace GeoLocationCityDetector.Services
+namespace GeoDistrictDetector.Services
 {
     /// <summary>
     /// 地理位置服务接口
     /// </summary>
-    public interface IGeoLocationService
+    public interface IGeoDistrictService
     {
         /// <summary>
         /// 根据坐标点查找所属城市
         /// </summary>
         /// <param name="point">坐标点</param>
         /// <returns>城市信息，如果未找到返回null</returns>
-        Task<CityInfo?> FindCityByPointAsync(GeoPoint point);
+        Task<District?> FindDistrictByPointAsync(GeoPoint point);
 
         /// <summary>
         /// 加载城市数据
         /// </summary>
         /// <param name="dataFilePath">数据文件路径</param>
         /// <returns></returns>
-        Task LoadCityDataAsync(string dataFilePath);
+        Task LoadDistrictDataAsync(string dataFilePath);
 
         /// <summary>
         /// 获取所有已加载的城市数据
         /// </summary>
         /// <returns>城市列表</returns>
-        List<CityInfo> GetAllCities();
+        List<District> GetAllDistricts();
     }
 }
